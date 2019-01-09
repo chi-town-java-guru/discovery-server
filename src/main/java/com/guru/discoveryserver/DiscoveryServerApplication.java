b81f1da177879ac94cf59c51309f713934b94ae5
Gurunath Sugavanam <gurunath.sugavanam@gmail.com>
2019-01-09 02:45:08 -0600

    added configs

--- src/main/java/com/guru/discoveryserver/DiscoveryServerApplication.java
+++ src/main/java/com/guru/discoveryserver/DiscoveryServerApplication.java
@@ -2,7 +2,9 @@ package com.guru.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
@@ -11,4 +13,3 @@ public class DiscoveryServerApplication {
 	}
 
 }
-
